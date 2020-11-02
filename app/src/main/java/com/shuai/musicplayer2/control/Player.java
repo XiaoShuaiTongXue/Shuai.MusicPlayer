0f1fda066d5e4f673b6ea1de721d70f2dde8666e
xiaoshuai <2236065790@qq.com>
2020-11-02 22:08:02 +0800

    完成动画，高斯模糊使用图片，未覆盖全屏

--- app/src/main/java/com/shuai/musicplayer2/control/Player.java
+++ app/src/main/java/com/shuai/musicplayer2/control/Player.java
@@ -1,25 +1,48 @@
 package com.shuai.musicplayer2.control;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.shuai.musicplayer2.R;
 import com.shuai.musicplayer2.api.Api;
 import com.shuai.musicplayer2.domain.MusicInfo;
+import com.shuai.musicplayer2.utils.FastBlur;
 import com.shuai.musicplayer2.utils.RetrofitManager;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,6 +57,8 @@ public class Player extends AppCompatActivity {
     private String mMusicId;
     private Call<MusicInfo> mTask;
     private MusicInfo mMusicInfo;
+    private ImageView mPic;
+    private ObjectAnimator mRotation;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -64,11 +89,30 @@ public class Player extends AppCompatActivity {
         if (songsBean.getMv()==0){
             mv.setVisibility(View.GONE);
         }
-        ImageView pic= findViewById(R.id.player_pic);
-        Glide.with(pic.getContext()).load(songsBean.getAl().getPicUrl()).into(pic);
+        mPic = findViewById(R.id.player_pic);
+        //设置背景图片
+        Glide.with(mPic.getContext())
+                .asBitmap()
+                .load(songsBean.getAl().getPicUrl())
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        mPic.setImageBitmap(resource);
+                        blur(resource, findViewById(R.id.content));
+                    }
+                });
+        //设置旋转动画
+        mRotation = ObjectAnimator
+                .ofFloat(mPic, "rotation", 0,360)
+                .setDuration(15000);
+        mRotation.setRepeatCount(Animation.INFINITE);
+        mRotation.setInterpolator(new LinearInterpolator());
+        mRotation.start();
     }
 
+
     private void initView() {
+        //根据json的内容设置页面内容
         mRetrofit = RetrofitManager.getRetrofit();
         mApi = mRetrofit.create(Api.class);
         mTask = mApi.getMusicInfo(mMusicId);
@@ -87,4 +131,44 @@ public class Player extends AppCompatActivity {
             }
         });
     }
+
+
+    @SuppressLint("NewApi")
+    private void blur(Bitmap bkg, View view) {
+        long startMs = System.currentTimeMillis();
+        float scaleFactor = 40;//图片缩放比例；
+        float radius = 10;//模糊程度
+
+        Bitmap overlay = Bitmap.createBitmap(
+                (int) (view.getMeasuredWidth() / scaleFactor),
+                (int) (view.getMeasuredHeight() / scaleFactor),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(overlay);
+        canvas.translate(-view.getLeft() / scaleFactor, -view.getTop()/ scaleFactor);
+        canvas.scale(1 / scaleFactor, 1 / scaleFactor);
+        Paint paint = new Paint();
+        paint.setFlags(Paint.FILTER_BITMAP_FLAG);
+        canvas.drawBitmap(bkg, 0, 0, paint);
+
+
+        overlay = FastBlur.doBlur(overlay, (int) radius, true);
+        view.setBackground(new BitmapDrawable(getResources(), overlay));
+        /**
+         * 打印高斯模糊处理时间，如果时间大约16ms，用户就能感到到卡顿，时间越长卡顿越明显，如果对模糊完图片要求不高，可是将scaleFactor设置大一些。
+         */
+        Log.i("jerome", "blur time:" + (System.currentTimeMillis() - startMs));
+    }
+
+//    /**
+//     * 获取系统状态栏和软件标题栏，部分软件没有标题栏，看自己软件的配置；
+//     * @return
+//     */
+//    private int getOtherHeight() {
+//        Rect frame = new Rect();
+//        getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+//        int statusBarHeight = frame.top;
+//        int contentTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+//        int titleBarHeight = contentTop - statusBarHeight;
+//        return statusBarHeight + titleBarHeight;
+//    }
 }
--- app/src/main/java/com/shuai/musicplayer2/service/PlayerService.java
+++ app/src/main/java/com/shuai/musicplayer2/service/PlayerService.java
@@ -2,14 +2,141 @@ package com.shuai.musicplayer2.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class PlayerService extends Service {
+    private static final String TAG = "player";
+    public MediaPlayer mediaPlayer;
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new MyBinder() ;
+    }
+
+    class MyBinder extends Binder {
+        public void plays(String path){
+            play(path);
+        }
+        public void pauses(){
+            pause();
+        }
+        public void replays(String path){
+            replay(path);
+        }
+        public void stops(){
+            stop();
+        }
+        public int getCurrentPosition(){
+            return getCurrentProgress();
+        }
+        public int getMusicWith(){
+            return getMusicLength();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    //播放音乐
+    public void play(String path){
+        try{
+            if (mediaPlayer == null){
+                //创建一个播放器
+                mediaPlayer = new MediaPlayer();
+                //指定播放器为音频文件
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                //指定播放路径
+                mediaPlayer.setDataSource(path);
+                //准备播放
+                mediaPlayer.prepare();
+                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        //开始播放
+                        mediaPlayer.start();
+                    }
+                });
+            }
+            else {
+                //获取当前进度
+                int position = getCurrentProgress();
+                //从指定位置播放音频
+                mediaPlayer.seekTo(position);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+            }
+        }catch (Exception e){
+            Log.i(TAG,e.toString());
+        }
+    }
+
+    //暂停音乐
+    public void pause(){
+        if (mediaPlayer!=null && mediaPlayer.isPlaying()){
+            Toast.makeText(this, "音乐已暂停", Toast.LENGTH_SHORT).show();
+            mediaPlayer.pause();
+        }else if(mediaPlayer != null && (!mediaPlayer.isPlaying())){
+            mediaPlayer.start();
+        }
+    }
+
+    //重新播放音乐
+    public void replay(String path){
+        if(mediaPlayer != null){
+            Toast.makeText(this, "重新播放音乐", Toast.LENGTH_SHORT).show();
+            mediaPlayer.seekTo(0);
+            try {
+                mediaPlayer.prepare();
+            }catch (Exception e){
+                Log.i(TAG,e.toString());
+            }
+            mediaPlayer.start();
+        }
+    }
+
+    //停止音乐
+    public void stop(){
+        if(mediaPlayer != null){
+            Toast.makeText(this, "停止播放", Toast.LENGTH_SHORT).show();
+            mediaPlayer.stop();
+            //释放资源
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }else {
+            Toast.makeText(this, "已经停止音乐", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //获取资源文件长度
+    public int getMusicLength(){
+        if(mediaPlayer!=null){
+            return mediaPlayer.getDuration();
+        }
+        return 0;
+    }
+
+    //获取当前进度
+    public int getCurrentProgress(){
+        if(mediaPlayer != null ){
+            return mediaPlayer.getCurrentPosition();
+        }
+        return 0;
+    }
+
+    @Override
+    public void onDestroy() {
+        stop();
+        super.onDestroy();
     }
 }
